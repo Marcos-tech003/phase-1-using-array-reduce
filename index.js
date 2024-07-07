77e41904bd52668df80fea43b4c3d6beafad1da0
-Mi.c3- <marcos.wainaina@student.moringa.school.com>
2024-07-07 15:19:10 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+const totalBatteries =batteryBatches.reduce((sum ,num)=>{
+    return sum + num;
+},0)
+
+console.log(total)
 // Code your solution here
